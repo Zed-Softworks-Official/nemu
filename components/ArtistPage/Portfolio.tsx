1bdf1c779bb17314aa7879d04c6dc221bd420b4b
Kevin Zoltany <kzoltany@gmail.com>
2023-09-22 19:56:26 -0700

    Portfolio pulling items from S3

--- components/ArtistPage/Portfolio.tsx
+++ components/ArtistPage/Portfolio.tsx
@@ -1,31 +1,15 @@
 import React from "react";
 
-import { PrismaClient } from "@prisma/client";
-import { AWSLocations, S3GetSignedURL } from "@/helpers/s3";
 import { PortfolioItem } from "@/helpers/portfolio";
 
 export default async function Portfolio({ handle, id }: { handle: string, id: string }) {
-    let prisma = new PrismaClient();
-
-    let portfolio = await prisma.portfolio.findMany({
-        where: {
-            auth0id: id
-        }
-    });
-
-    let items: PortfolioItem[] = [];
-    for (let i = 0; i < portfolio.length; i++) {
-        let url = await S3GetSignedURL(handle, AWSLocations.Portfolio, portfolio[i].image);
-        items.push({
-            signed_url: url,
-            name: portfolio[i].name
-        });
-    }
+    let res = await fetch('/api/artist/portfolio/' + handle + '/' + id);
+    let data = await res.json();
 
     return (
     <div className="columns-1 md:columns-2 lg:columns-3 xl:columns-4">
-        {items.map((item: PortfolioItem) => {
-        (
+        {data.portfolio_items.map( (item: PortfolioItem) => {
+        return (
             <div className="w-fit h-fit m-5">
                 <img src={item.signed_url} alt={item.name} className="rounded-3xl" />
             </div>
