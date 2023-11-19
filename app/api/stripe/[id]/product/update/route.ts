ad7afb68422b6f84df2a333a9e267b541600c432
Kevin Zoltany <kzoltany@gmail.com>
2023-11-18 23:29:06 -0800

    Updated formatting for product update route

--- app/api/stripe/[id]/product/update/route.ts
+++ app/api/stripe/[id]/product/update/route.ts
@@ -3,12 +3,18 @@ import Stripe from 'stripe'
 import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
-import { StripeCreatePrice, StripeGetPriceInfo, StripeGetPrices, StripeGetRawProductInfo, StripeUpdateProduct } from '@/helpers/stripe'
+import {
+    StripeCreatePrice,
+    StripeGetPriceInfo,
+    StripeGetPrices,
+    StripeGetRawProductInfo,
+    StripeUpdateProduct
+} from '@/helpers/stripe'
 import { NemuResponse, StatusCode } from '@/helpers/api/request-inerfaces'
 
 /**
  * Updates an item based on a given product id
- * 
+ *
  * @param id - the product id
  */
 export async function POST(req: Request, { params }: { params: { id: string } }) {
@@ -23,33 +29,43 @@ export async function POST(req: Request, { params }: { params: { id: string } })
     })
 
     // Get the product from stripe
-    const product = await StripeGetRawProductInfo(db_product?.product!, db_product?.stripeAccId!)
+    const product = await StripeGetRawProductInfo(
+        db_product?.product!,
+        db_product?.stripeAccId!
+    )
 
     // Get price information
-    const price = (await StripeGetPriceInfo(product.default_price?.toString()!, db_product?.stripeAccId!)).unit_amount
+    const price = (
+        await StripeGetPriceInfo(
+            product.default_price?.toString()!,
+            db_product?.stripeAccId!
+        )
+    ).unit_amount
     const all_prices = await StripeGetPrices(db_product?.stripeAccId!)
 
     // Create variables for new values
     let updated_values: Stripe.ProductUpdateParams = {}
     let create_new_price: boolean = false
     let new_price_value: number | undefined
-    
+
     // Check to see which values have been updated
     formData.forEach((value, key) => {
         switch (key) {
             case 'product_name':
-                updated_values.name = value != product.name ? value as string : undefined
+                updated_values.name =
+                    value != product.name ? (value as string) : undefined
                 break
             case 'product_description':
-                updated_values.description = value != product.description ? value as string : undefined
+                updated_values.description =
+                    value != product.description ? (value as string) : undefined
                 break
             case 'product_price':
                 // Check if the price changed
-                if (Number(value) != (price! / 100)) {
+                if (Number(value) != price! / 100) {
                     // Loop through all of our prices in case we have a price object with that value
                     for (let price_object of all_prices.data) {
                         // If we find a price with that value then we set the new default price to that price id
-                        if (Number(value) == (price_object.unit_amount! / 100)) {
+                        if (Number(value) == price_object.unit_amount! / 100) {
                             updated_values.default_price = price_object.id
                             return
                         }
@@ -60,17 +76,25 @@ export async function POST(req: Request, { params }: { params: { id: string } })
                     new_price_value = Number(value)
                 }
                 break
-            }
-        })
+        }
+    })
     // Create new price object if we need too
     if (create_new_price) {
-        await StripeCreatePrice(new_price_value!, db_product?.product!, db_product?.stripeAccId!).then((new_price) => {
+        await StripeCreatePrice(
+            new_price_value!,
+            db_product?.product!,
+            db_product?.stripeAccId!
+        ).then((new_price) => {
             updated_values.default_price = new_price.id
         })
     }
-        
+
     // Update changed values on stripe
-    await StripeUpdateProduct(db_product?.product!, updated_values, db_product?.stripeAccId!)
+    await StripeUpdateProduct(
+        db_product?.product!,
+        updated_values,
+        db_product?.stripeAccId!
+    )
 
     return NextResponse.json<NemuResponse>({
         status: StatusCode.Success
