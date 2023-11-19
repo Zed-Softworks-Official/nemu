1a746fe04c4d03b25917bcc1c86d85c71e5ac80a
Kevin Zoltany <kzoltany@gmail.com>
2023-11-18 23:28:03 -0800

    Price changes now work on products

--- app/api/stripe/[id]/product/update/route.ts
+++ app/api/stripe/[id]/product/update/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
-import { StripeGetPriceInfo, StripeGetRawProductInfo, StripeUpdateProduct } from '@/helpers/stripe'
+import { StripeCreatePrice, StripeGetPriceInfo, StripeGetPrices, StripeGetRawProductInfo, StripeUpdateProduct } from '@/helpers/stripe'
 import { NemuResponse, StatusCode } from '@/helpers/api/request-inerfaces'
 
 /**
@@ -27,9 +27,14 @@ export async function POST(req: Request, { params }: { params: { id: string } })
 
     // Get price information
     const price = (await StripeGetPriceInfo(product.default_price?.toString()!, db_product?.stripeAccId!)).unit_amount
+    const all_prices = await StripeGetPrices(db_product?.stripeAccId!)
 
-    // Check to see which values have been updated
+    // Create variables for new values
     let updated_values: Stripe.ProductUpdateParams = {}
+    let create_new_price: boolean = false
+    let new_price_value: number | undefined
+    
+    // Check to see which values have been updated
     formData.forEach((value, key) => {
         switch (key) {
             case 'product_name':
@@ -39,11 +44,31 @@ export async function POST(req: Request, { params }: { params: { id: string } })
                 updated_values.description = value != product.description ? value as string : undefined
                 break
             case 'product_price':
-                //updated_values (price! / 100)
-                break
-        }
-    })
+                // Check if the price changed
+                if (Number(value) != (price! / 100)) {
+                    // Loop through all of our prices in case we have a price object with that value
+                    for (let price_object of all_prices.data) {
+                        // If we find a price with that value then we set the new default price to that price id
+                        if (Number(value) == (price_object.unit_amount! / 100)) {
+                            updated_values.default_price = price_object.id
+                            return
+                        }
+                    }
 
+                    // If we haven't found it set a flag to create a new price
+                    create_new_price = true
+                    new_price_value = Number(value)
+                }
+                break
+            }
+        })
+    // Create new price object if we need too
+    if (create_new_price) {
+        await StripeCreatePrice(new_price_value!, db_product?.product!, db_product?.stripeAccId!).then((new_price) => {
+            updated_values.default_price = new_price.id
+        })
+    }
+        
     // Update changed values on stripe
     await StripeUpdateProduct(db_product?.product!, updated_values, db_product?.stripeAccId!)
 
--- helpers/stripe.ts
+++ helpers/stripe.ts
@@ -164,6 +164,19 @@ export async function StripeGetPurchasePage(
     )
 }
 
+export async function StripeCreatePrice(amount: number, product_id: string, stripe_account: string) {
+    return await stripe.prices.create(
+        {
+            currency: 'usd',
+            unit_amount: amount * 100,
+            product: product_id
+        },
+        {
+            stripeAccount: stripe_account
+        }
+    )
+}
+
 /**
  * Finds the price info for a given price from a stripe account
  *
@@ -175,6 +188,21 @@ export async function StripeGetPriceInfo(price_id: string, stripe_account: strin
     return await stripe.prices.retrieve(price_id, {
         stripeAccount: stripe_account
     })
+}
+
+/**
+ * Collects all prices for a given stripe account
+ *
+ * @param {string} stripe_account - The stripe id to gather all of the prices
+ * @returns A list of all prices for a given account
+ */
+export async function StripeGetPrices(stripe_account: string) {
+    return await stripe.prices.list(
+        {},
+        {
+            stripeAccount: stripe_account
+        }
+    )
 }
 
 /**
