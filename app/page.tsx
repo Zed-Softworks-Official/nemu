d802e38a6642f022456ea042f90284f20dbf6e69
kzolt <kzoltany@gmail.com>
2023-12-03 13:01:54 -0800

    Updated Loading, Logo, and Aritsts Wanted Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import DefaultPageLayout from './(default)/layout'
 import RandomArtists from '@/components/homepage/random-artists'
+import Image from 'next/image'
 
 export default function Home() {
     return (
@@ -8,8 +9,8 @@ export default function Home() {
             <main className="flex flex-wrap container mx-auto">
                 <div className="w-full text-white">
                     <Link href={'/artists'} className="w-full">
-                        <div className="bg-gradient-to-r from-primary to-azure rounded-3xl py-10 px-20">
-                            <div className="inline-block">
+                        <div className="bg-gradient-to-r from-primary to-azure rounded-3xl grid grid-cols-2">
+                            <div className="inline-block py-10 px-20">
                                 <h1>Artists Wanted!</h1>
                                 <p>Something Clever Here</p>
 
@@ -17,9 +18,14 @@ export default function Home() {
                                     Click Here to become an artist!
                                 </span>
                             </div>
-
-                            <div className="float-right">
-                                <p>Nemu With Artist Wanted go here</p>
+                            <div className="flex justify-end items-end">
+                                <Image
+                                    src={'/artists-wanted.png'}
+                                    alt='Nemu with a sign that says "artists wanted"'
+                                    width={350}
+                                    height={350}
+                                    className="pr-20"
+                                />
                             </div>
                         </div>
                     </Link>
--- components/Navigation/Standard/Logo.tsx
+++ components/Navigation/Standard/Logo.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+'use client'
 
-import Link from "next/link";
+import React from 'react'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { useThemeContext } from '@/components/theme/theme-context'
 
 export default function Logo() {
+    const { theme } = useThemeContext()
+
     return (
         <div className="logo inline">
-            <h1>
-                <Link href={"/"}>
-                    Nemu
-                </Link>
-            </h1>
+            <Link href={'/'}>
+                {theme == 'dark' ? (
+                    <Image src={'/logo-dark.png'} alt="Nemu" width={150} height={200} draggable={false} />
+                ) : (
+                    <Image src={'/logo-light.png'} alt="Nemu" width={150} height={200} draggable={false}/>
+                )}
+            </Link>
         </div>
     )
 }
