0922579d9852be01d0fdaef76c2bc8c245442796
Kevin Zoltany <kzoltany@gmail.com>
2023-12-10 21:15:30 -0800

    Updated artist verification

--- app/api/artist/route.ts
+++ app/api/artist/route.ts
@@ -7,29 +7,35 @@ import { NemuResponse, StatusCode } from '@/helpers/api/request-inerfaces'
 export async function POST(req: Request) {
     const verification_data: ArtistVerificationData = await req.json()
 
-    const new_user = await prisma.artist.create({
+    const new_artist = await prisma.user.update({
+        where: {
+            id: verification_data.user_id
+        },
         data: {
-            userId: verification_data.user_id,
-            stripeAccId: '',
+            artist: {
+                create: {
+                    stripeAccId: '',
 
-            handle: verification_data.requested_handle,
-            about: 'Peko Peko',
-            location:
-                verification_data.location == ''
-                    ? "Nemu's Basment"
-                    : verification_data.location,
-            twitter: verification_data.twitter,
-            pixiv: verification_data.pixiv,
-            website: '',
-            headerPhoto: '',
-            profilePhoto: ''
+                    handle: verification_data.requested_handle,
+                    about: 'Peko Peko',
+                    location:
+                        verification_data.location == ''
+                            ? "Nemu's Basment"
+                            : verification_data.location,
+                    twitter: verification_data.twitter,
+                    pixiv: verification_data.pixiv,
+                    website: '',
+                    headerPhoto: '',
+                    profilePhoto: ''
+                }
+            }
         }
     })
 
-    if (!new_user) {
+    if (!new_artist) {
         return NextResponse.json<NemuResponse>({
             status: StatusCode.InternalError,
-            message: 'Could not create user!'
+            message: 'Could not create artist!'
         })
     }
 
--- helpers/artist-verification.ts
+++ helpers/artist-verification.ts
@@ -1,3 +1,5 @@
+import { NemuResponse, StatusCode } from './api/request-inerfaces'
+
 /**
  * Verification Data used to create the artist object
  */
@@ -13,44 +15,49 @@ export interface ArtistVerificationData {
 
 /**
  * Creates an Artist using an artist code
- * 
+ *
  * @param {string} code - The artist code used
  * @param {string} id - The id of the user to become an artist
- * @returns 
+ * @returns
  */
-export async function ArtistCodeVerification(code: string, id: string, verification_data: ArtistVerificationData) {
+export async function ArtistCodeVerification(
+    code: string,
+    id: string,
+    verification_data: ArtistVerificationData
+) {
     // Check to see if the artist code exists using the api
-    let response = await fetch(`/api/artist/code/${code}`, {
+    const code_response = await fetch(`/api/artist/code/${code}`, {
         method: 'post'
-    });
-    
-    // Check so see if it's a valid code and return if it doesn't
-    let validCode: boolean = (await response.json()).success
-    if (validCode == false) {
-        return false;
-    }
-    
-    // Adds Artist Role to User
-    let updated = await fetch(`/api/user/${id}/role/Artist`, {
-        method: 'post'
-    });
+    })
 
-    if (!updated) {
+    // Check so see if it's a valid code and return if it doesn't
+    let validCode: boolean =
+        ((await code_response.json()) as NemuResponse).status != StatusCode.Success
+    if (validCode == false) {
+        return false
+    }
+
+    // Adds Artist Role to User
+    const role_updated = await fetch(`/api/user/${id}/role/artist`, {
+        method: 'post'
+    })
+
+    if (((await role_updated.json()) as NemuResponse).status != StatusCode.Success) {
         return false
     }
 
     // Create artist inside database
-    let artistCreated = await fetch(`/api/artist`, {
+    const artist_created = await fetch(`/api/artist`, {
         method: 'post',
         body: JSON.stringify(verification_data)
     })
 
-    if (!artistCreated) {
+    if ((((await artist_created.json()) as NemuResponse).status != StatusCode.Success)) {
         return false
     }
 
     // Send Email to user
-
+    
 
     // Delete the code
     await fetch(`/api/artist/code/${code}/delete`, {
@@ -60,14 +67,16 @@ export async function ArtistCodeVerification(code: string, id: string, verificat
     return true
 }
 
-
 /**
  * Creates an artist using twitter verification
- * 
+ *
  * @param id - User id for the user to become an artist
  * @param verification_data - The provided by the user for their desired profile
  */
-export async function ArtistTwitterVerification(id: string, verification_data: ArtistVerificationData) {
+export async function ArtistTwitterVerification(
+    id: string,
+    verification_data: ArtistVerificationData
+) {
     // Create a data object in the database
     const response = await fetch('/api/artist/verify', {
         method: 'post',
@@ -75,5 +84,4 @@ export async function ArtistTwitterVerification(id: string, verification_data: A
     })
 
     // Send Email to user
-
 }
