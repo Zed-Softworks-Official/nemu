9556ccef9d229022102ec5c156f1c0cd54ae86e9
Kevin Zoltany <kzoltany@gmail.com>
2024-06-12 02:09:18 -0700

    random commissions on home page

--- src/app/(standard)/page.tsx
+++ src/app/(standard)/page.tsx
@@ -7,11 +7,24 @@ import { Suspense } from 'react'
 import NemuImage from '~/components/nemu-image'
 import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar'
 import Loading from '~/components/ui/loading'
+import Masonry from '~/components/ui/masonry'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs'
+import { NemuImageData } from '~/core/structures'
+import { get_blur_data } from '~/lib/blur_data'
+import { format_to_currency } from '~/lib/utils'
 import { db } from '~/server/db'
 import { artists } from '~/server/db/schema'
 
 type RandomArtistReturnType = InferSelectModel<typeof artists> & { user: User }
+type RandomCommissionReturnType = {
+    id: string
+    title: string
+    description: string
+    featured_image: NemuImageData
+    price: string
+    handle: string
+    slug: string
+}
 
 const get_random_artists = unstable_cache(
     async () => {
@@ -27,8 +40,41 @@ const get_random_artists = unstable_cache(
 
         return result
     },
-    ['random-artists'],
-    { tags: ['random-artists'] }
+    ['random_artists'],
+    { tags: ['random_artists'], revalidate: 60 }
+)
+
+const get_random_commissions = unstable_cache(
+    async () => {
+        const commissions = await db.query.commissions.findMany({
+            with: {
+                artist: true
+            }
+        })
+
+        const result: RandomCommissionReturnType[] = []
+        for (let i = 0; i < commissions.length; i++) {
+            result.push({
+                id: commissions[i]?.id!,
+                title: commissions[i]?.title!,
+                description: commissions[i]?.description!,
+                featured_image: {
+                    url: commissions[i]?.images[0]?.url!,
+                    blur_data: await get_blur_data(commissions[i]?.images[0]?.url!)
+                },
+                handle: commissions[i]?.artist.handle!,
+                price: format_to_currency(commissions[i]?.price!),
+                slug: commissions[i]?.slug!
+            })
+        }
+
+        return result
+    },
+    ['random_commissions'],
+    {
+        tags: ['random_comissions'],
+        revalidate: 60
+    }
 )
 
 export default function Home() {
@@ -55,15 +101,16 @@ export default function Home() {
                     />
                 </div>
             </Link>
-            <Tabs defaultValue="all">
+            <Tabs defaultValue="commissions">
                 <TabsList className="w-full justify-start bg-base-200">
-                    <TabsTrigger value="all">All</TabsTrigger>
+                    <TabsTrigger value="commissions">Commissions</TabsTrigger>
                     <TabsTrigger value="artists">Artists</TabsTrigger>
                     <TabsTrigger value="portfolio">Portfolio</TabsTrigger>
-                    <TabsTrigger value="commissions">Commissions</TabsTrigger>
                 </TabsList>
-                <TabsContent value="all">
-                    <>All</>
+                <TabsContent value="commissions">
+                    <Suspense fallback={<Loading />}>
+                        <CommissionsList />
+                    </Suspense>
                 </TabsContent>
                 <TabsContent value="artists">
                     <Suspense fallback={<Loading />}>
@@ -71,7 +118,6 @@ export default function Home() {
                     </Suspense>
                 </TabsContent>
                 <TabsContent value="portfolio"></TabsContent>
-                <TabsContent value="commissisons"></TabsContent>
             </Tabs>
         </main>
     )
@@ -105,6 +151,43 @@ async function ArtistsList() {
                         <h1 className="text-2xl font-bold">{artist.handle}</h1>
                         <p>{artist.about}</p>
                     </div>
+                </Link>
+            ))}
+        </div>
+    )
+}
+
+async function CommissionsList() {
+    const commissions = await get_random_commissions()
+
+    return (
+        <div
+            className={'columns-1 gap-5 space-y-5 sm:columns-3 lg:columns-4 xl:columns-5'}
+        >
+            {commissions.map((commission) => (
+                <Link
+                    key={commission.id}
+                    href={`/@${commission.handle}/commission/${commission.slug}`}
+                    className="group relative animate-pop-in rounded-xl transition-all duration-150 ease-in-out"
+                >
+                    <div className="absolute flex h-full w-full flex-col items-center rounded-xl bg-base-300/80 p-5 opacity-0 transition-all duration-200 ease-in-out group-hover:opacity-100">
+                        <div>
+                            <h1 className="text-2xl font-bold">{commission.title}</h1>
+                            <p className="text-base-content/80">
+                                {commission.description}
+                            </p>
+                        </div>
+                        <div className="flex h-full items-end justify-start"></div>
+                    </div>
+                    <NemuImage
+                        src={commission.featured_image.url!}
+                        alt="Commission Image"
+                        width={200}
+                        height={200}
+                        className="w-full rounded-xl"
+                        placeholder="blur"
+                        blurDataURL={commission.featured_image.blur_data}
+                    />
                 </Link>
             ))}
         </div>
