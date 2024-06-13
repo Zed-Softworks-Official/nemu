ef0c41696e272c0783ad2fcdf0dd8c62c9b487cb
Kevin Zoltany <kzoltany@gmail.com>
2024-06-12 21:44:24 -0700

    modfied share button and added portfolio image fetching

--- src/components/ui/share-button.tsx
+++ src/components/ui/share-button.tsx
@@ -1,11 +1,9 @@
 'use client'
 
 import { Share2Icon } from 'lucide-react'
-import { useTheme } from 'next-themes'
 import { usePathname } from 'next/navigation'
 import { toast } from 'sonner'
 import { Button } from '~/components/ui/button'
-import { env } from '~/env'
 
 export default function ShareButton() {
     const pathname = usePathname()
@@ -15,10 +13,10 @@ export default function ShareButton() {
             variant={'outline'}
             onMouseDown={() => {
                 navigator.clipboard.writeText('http://localhost:3000' + pathname)
-                toast('Copied to clipboard' )
+                toast.info('Copied to clipboard')
             }}
         >
-            <Share2Icon className="w-6 h-6" />
+            <Share2Icon className="h-6 w-6" />
             Share
         </Button>
     )
