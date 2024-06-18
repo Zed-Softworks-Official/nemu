adfcd8cc6126a41cb47415deb671db8acf595efd
Kevin Zoltany <kzoltany@gmail.com>
2024-06-17 21:29:47 -0700

    Finished Artist Dashboard home

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { clerkClient, currentUser } from '@clerk/nextjs/server'
-import { desc, eq } from 'drizzle-orm'
+import { and, desc, eq, gte } from 'drizzle-orm'
 import { unstable_cache } from 'next/cache'
 import { Suspense } from 'react'
 import SalesChart from '~/components/dashboard/sales-chart'
@@ -14,7 +14,7 @@ import {
 } from '~/components/ui/card'
 import Loading from '~/components/ui/loading'
 import { InvoiceStatus, SalesData } from '~/core/structures'
-import { format_to_currency } from '~/lib/utils'
+import { calculate_percentage_change, format_to_currency } from '~/lib/utils'
 
 import { db } from '~/server/db'
 import { artists, commissions, invoices, requests } from '~/server/db/schema'
@@ -42,6 +42,7 @@ type RecentSales = {
     sales_data: SalesData[]
     total_revenue: SalesStat
     total_sales: SalesStat
+    total_requests: SalesStat
 }
 
 const get_recent_requests = unstable_cache(
@@ -130,63 +131,44 @@ const get_recent_sales = unstable_cache(
             return undefined
         }
 
+        // Get the date 6 months prior
+        const start_date = new Date()
+        const current_date = new Date()
+        start_date.setMonth(start_date.getMonth() - 6)
+
+        // Get the requests from the db
+        const db_requests = await db
+            .select()
+            .from(commissions)
+            .where(eq(commissions.artist_id, artist_id))
+            .leftJoin(
+                requests,
+                and(
+                    eq(requests.commission_id, commissions.id),
+                    gte(requests.created_at, start_date)
+                )
+            )
+
         // Fetch the sales data from stripe
         const charges = await stripe.charges.list(
-            {},
+            {
+                created: {
+                    gte: Math.floor(start_date.getTime() / 1000)
+                }
+            },
             { stripeAccount: artist.stripe_account }
         )
 
         // Create the sales data object for the chart
-        const sales_data: SalesData[] = [
-            {
-                month: 'January',
-                total_sales: 0
-            },
-            {
-                month: 'February',
-                total_sales: 0
-            },
-            {
-                month: 'March',
-                total_sales: 0
-            },
-            {
-                month: 'April',
-                total_sales: 0
-            },
-            {
-                month: 'May',
-                total_sales: 0
-            },
-            {
-                month: 'June',
-                total_sales: 0
-            },
-            {
-                month: 'July',
-                total_sales: 0
-            },
-            {
-                month: 'August',
-                total_sales: 0
-            },
-            {
-                month: 'September',
-                total_sales: 0
-            },
-            { month: 'October', total_sales: 0 },
-            {
-                month: 'November',
-                total_sales: 0
-            },
-            {
-                month: 'December',
-                total_sales: 0
-            }
-        ]
+        const sales_data: Record<string, SalesData> = {}
 
-        // Calculate the total revenue
+        // Loop through the charges and add the amount to the correct month in the sales data
         let total_revenue_amount = 0
+        let previous_total_revenue_amount = 0
+
+        let total_sales_amount = 0
+        let previous_sales_amount = 0
+
         for (const charge of charges.data) {
             // Check if the charge is a successful charge
             if (charge.status !== 'succeeded') {
@@ -197,27 +179,81 @@ const get_recent_sales = unstable_cache(
             const charge_date = new Date(0)
             charge_date.setUTCSeconds(charge.created)
 
-            // Add the amount to the correct month in the sales data
-            const month_index = sales_data.findIndex(
-                (month) =>
-                    month.month === charge_date.toLocaleString('en-US', { month: 'long' })
-            )
-            sales_data[month_index]!.total_sales += charge.amount / 100
+            const charge_month = charge_date.toLocaleString('en-US', { month: 'long' })
+            sales_data[charge_month] = {
+                month: charge_date.toLocaleString('en-US', { month: 'long' }),
+                total_sales: sales_data[charge_month]
+                    ? (sales_data[charge_month].total_sales += charge.amount / 100)
+                    : charge.amount / 100
+            }
+
+            // Increment the total sales amount if the sale is from the current month
+            if (current_date.getMonth() === charge_date.getMonth()) {
+                total_sales_amount += 1
+            }
 
             // Add the amount to the total revenue
             total_revenue_amount += charge.amount / 100
+
+            // if the charge is from the previous month of our current month, add it to the previous revenue total
+            const previous_date = new Date(
+                charge_date.getMonth() - 1,
+                charge_date.getDate()
+            )
+            const previous_month = previous_date.getMonth()
+
+            if (current_date.getMonth() - 1 === previous_month) {
+                previous_total_revenue_amount += charge.amount / 100
+                previous_sales_amount += 1
+            }
+        }
+
+        // Calculate the percentage change for the requests
+        let total_requests_amount = 0
+        let previous_total_requests_amount = 0
+        for (const { request } of db_requests) {
+            // Check if the request exists
+            if (!request) {
+                continue
+            }
+
+            // Check if the request is from the current month, then add it to the total for the current month
+            // otherwise check if the request is from the previous month, then add it to the previous month total
+            const request_date = new Date(request.created_at)
+
+            if (current_date.getMonth() === request_date.getMonth()) {
+                total_requests_amount += 1
+            } else if (current_date.getMonth() - 1 === request_date.getMonth()) {
+                previous_total_requests_amount += 1
+            }
         }
 
         // Construct the recent sales data
         const result: RecentSales = {
-            sales_data: sales_data,
+            sales_data: Object.entries(sales_data).map(([key, value]) => ({
+                month: key,
+                total_sales: value.total_sales
+            })),
             total_revenue: {
                 count: format_to_currency(total_revenue_amount),
-                change: '+20.1%'
+                change: calculate_percentage_change(
+                    total_revenue_amount,
+                    previous_total_revenue_amount
+                )
             },
             total_sales: {
-                count: charges.data.length,
-                change: '+19%'
+                count: total_sales_amount,
+                change: calculate_percentage_change(
+                    total_sales_amount,
+                    previous_sales_amount
+                )
+            },
+            total_requests: {
+                count: total_requests_amount,
+                change: calculate_percentage_change(
+                    total_requests_amount,
+                    previous_total_requests_amount
+                )
             }
         }
 
@@ -374,9 +410,11 @@ async function TotalSales() {
                 </div>
 
                 <div className="stat">
-                    <div className="stat-title"></div>
-                    <div className="stat-value">1,200</div>
-                    <div className="stat-desc">↘︎ 90 (14%)</div>
+                    <div className="stat-title">Total Requests</div>
+                    <div className="stat-value">{recent_sales.total_requests.count}</div>
+                    <div className="stat-desc">
+                        {recent_sales.total_requests.change} from last month
+                    </div>
                 </div>
             </div>
         </div>
