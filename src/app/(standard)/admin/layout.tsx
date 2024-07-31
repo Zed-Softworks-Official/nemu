b88179c0883fbf6864d4e4583ec9c164621d0807
Kevin Zoltany <kzoltany@gmail.com>
2024-07-30 23:28:10 -0700

    updated admin layout to use database role instead of clerk role

--- src/app/(standard)/admin/layout.tsx
+++ src/app/(standard)/admin/layout.tsx
@@ -1,12 +1,23 @@
 import { currentUser } from '@clerk/nextjs/server'
+import { eq } from 'drizzle-orm'
 import { redirect } from 'next/navigation'
 import { UserRole } from '~/core/structures'
+import { db } from '~/server/db'
+import { users } from '~/server/db/schema'
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
     const user = await currentUser()
 
-    if (!user || user.publicMetadata.role !== UserRole.Admin) {
-        return redirect('/')
+    if (!user) {
+        return redirect('/u/login')
+    }
+
+    const db_user = await db.query.users.findFirst({
+        where: eq(users.clerk_id, user.id)
+    })
+
+    if (!db_user || db_user.role !== UserRole.Admin) {
+        return redirect('/u/login')
     }
 
     return <>{children}</>
