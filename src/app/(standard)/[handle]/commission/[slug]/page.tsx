76d2bbb4844e2f4d625147ab85a31cf79b23328c
Kevin Zoltany <kzoltany@gmail.com>
2024-09-17 11:43:08 -0700

    Metadata for commissions page added

--- src/app/(standard)/[handle]/commission/[slug]/page.tsx
+++ src/app/(standard)/[handle]/commission/[slug]/page.tsx
@@ -1,13 +1,41 @@
+import type { Metadata } from 'next'
 import { Suspense } from 'react'
+
 import CommissionDisplay from '~/components/displays/commission/display'
 import Loading from '~/components/ui/loading'
 import { get_commission } from '~/server/db/query'
 
-export default function CommissionsPage({
-    params
-}: {
-    params: { handle: string; slug: string }
-}) {
+type Props = { params: { handle: string; slug: string } }
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const handle = params.handle.substring(3, params.handle.length + 1)
+    const commission = await get_commission(handle, params.slug)
+
+    if (!commission) {
+        return {}
+    }
+
+    const image = commission.images[0]?.url
+
+    if (!image) {
+        return {}
+    }
+
+    return {
+        title: `Nemu | @${handle}`,
+        description: `Check out ${handle}'s commission on Nemu!`,
+        twitter: {
+            title: `Nemu | @${handle}`,
+            description: `Check out ${handle}'s commission on Nemu!`,
+            images: [image]
+        },
+        openGraph: {
+            images: [image]
+        }
+    }
+}
+
+export default function CommissionsPage({ params }: Props) {
     const handle = params.handle.substring(3, params.handle.length + 1)
 
     return (
