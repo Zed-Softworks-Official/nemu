90a9a21407fb7c24c4d84c4c08f05e23f2eea4c0
Kevin Zoltany <kzoltany@gmail.com>
2024-10-27 13:09:22 -0700

    Deleted Portfolio and User Routes from TRPC, moved all verifications routes to next actions

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,6 @@
 import { createCallerFactory, createTRPCRouter } from '~/server/api/trpc'
 
-import { userRouter } from '~/server/api/routers/user'
 import { stripeRouter } from '~/server/api/routers/stripe'
-import { verificationRouter } from '~/server/api/routers/verification'
 import { formRouter } from '~/server/api/routers/form'
 import { requestRouter } from '~/server/api/routers/requests'
 import { kanbanRouter } from '~/server/api/routers/kanban'
@@ -13,12 +11,10 @@ import { kanbanRouter } from '~/server/api/routers/kanban'
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-    verification: verificationRouter,
     requests: requestRouter,
     form: formRouter,
     stripe: stripeRouter,
-    kanban: kanbanRouter,
-    user: userRouter
+    kanban: kanbanRouter
 })
 
 // export type definition of API
