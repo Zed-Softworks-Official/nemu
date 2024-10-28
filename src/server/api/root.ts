c32d47936f6fc8da611068f75c56804f87abd576
Kevin Zoltany <kzoltany@gmail.com>
2024-10-28 13:09:30 -0700

    Moved requests to next actions

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,15 +1,11 @@
 import { createCallerFactory, createTRPCRouter } from '~/server/api/trpc'
 
-import { requestRouter } from '~/server/api/routers/requests'
-
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({
-    requests: requestRouter
-})
+export const appRouter = createTRPCRouter({})
 
 // export type definition of API
 export type AppRouter = typeof appRouter
