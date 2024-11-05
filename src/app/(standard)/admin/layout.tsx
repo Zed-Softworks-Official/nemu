6b175f64c6b21abab0134dcd0b2e38c80b2d747e
Kevin Zoltany <kzoltany@gmail.com>
2024-11-04 21:57:49 -0800

    artist code revalidations on creation

--- src/app/(standard)/admin/layout.tsx
+++ src/app/(standard)/admin/layout.tsx
@@ -1,9 +1,7 @@
-import { currentUser } from '@clerk/nextjs/server'
-import { eq } from 'drizzle-orm'
 import { redirect } from 'next/navigation'
+import { currentUser } from '@clerk/nextjs/server'
+
 import { UserRole } from '~/core/structures'
-import { db } from '~/server/db'
-import { users } from '~/server/db/schema'
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
     const user = await currentUser()
@@ -12,12 +10,8 @@ export default async function Layout({ children }: { children: React.ReactNode }
         return redirect('/u/login')
     }
 
-    const db_user = await db.query.users.findFirst({
-        where: eq(users.clerk_id, user.id)
-    })
-
-    if (!db_user || db_user.role !== UserRole.Admin) {
-        return redirect('/u/login')
+    if (user.publicMetadata.role !== UserRole.Admin) {
+        return redirect('/')
     }
 
     return <>{children}</>
