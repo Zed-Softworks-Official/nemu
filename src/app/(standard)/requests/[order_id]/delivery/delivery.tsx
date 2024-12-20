86e2bdc4e404f26d4f54bda6bf3d32dfffd24135
Kevin Zoltany <kzoltany@gmail.com>
2024-12-19 18:53:22 -0800

    removed incompatible types from delivery

--- src/app/(standard)/requests/[order_id]/delivery/delivery.tsx
+++ src/app/(standard)/requests/[order_id]/delivery/delivery.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { FileArchiveIcon } from 'lucide-react'
-import Link from 'next/link'
 import NemuImage from '~/components/nemu-image'
 import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card'
 import Loading from '~/components/ui/loading'
@@ -38,67 +36,8 @@ export default function Delivery(props: { order_id: string }) {
                 <CardHeader>
                     <CardTitle>Downloads</CardTitle>
                 </CardHeader>
-                <CardContent>
-                    <DownloadPreview
-                        url={request.delivery.url}
-                        blur_data={request.delivery.blur_data}
-                        file_type={request.delivery.file_type}
-                        created_at={request.delivery.created_at}
-                    />
-                </CardContent>
+                <CardContent>{JSON.stringify(request.delivery)}</CardContent>
             </Card>
         </main>
-    )
-}
-
-function DownloadPreview(props: {
-    url: string
-    file_type: 'image' | 'zip'
-    blur_data?: string
-    created_at: Date
-}) {
-    if (props.file_type === 'image') {
-        return (
-            <div className="flex h-full w-full flex-row items-center justify-between gap-5">
-                <NemuImage
-                    src={props.url}
-                    alt="Download Preview"
-                    width={200}
-                    height={200}
-                    className="rounded-xl"
-                />
-                <div className="flex flex-col">
-                    <span className="text-base-content/60 text-sm">
-                        Download created at{' '}
-                        {new Date(props.created_at).toLocaleDateString()}
-                    </span>
-                    <Link
-                        href={props.url}
-                        className="btn btn-primary btn-wide text-white"
-                        download
-                    >
-                        Download
-                    </Link>
-                </div>
-            </div>
-        )
-    }
-
-    return (
-        <div className="flex h-full w-full flex-row items-center justify-between gap-5">
-            <FileArchiveIcon className="h-10 w-10" />
-            <div className="flex flex-col">
-                <span className="text-base-content/60 text-sm">
-                    Download created at {new Date(props.created_at).toLocaleDateString()}
-                </span>
-                <Link
-                    href={props.url}
-                    className="btn btn-primary btn-wide text-white"
-                    download
-                >
-                    Download
-                </Link>
-            </div>
-        </div>
     )
 }
