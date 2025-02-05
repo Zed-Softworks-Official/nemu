8f039801407bb5a2cf4662c48a66fbd15e94bd15
Kevin Zoltany <kzoltany@gmail.com>
2025-02-04 20:30:23 -0800

    Posthog ingest & Bug fixes

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,24 @@ const config = {
                 search: ''
             }
         ]
-    }
+    },
+    rewrites: async () => {
+        return [
+            {
+                source: '/ingest/static/:path*',
+                destination: 'https://us-assets.i.posthog.com/static/:path*'
+            },
+            {
+                source: '/ingest/:path*',
+                destination: 'https://us.i.posthog.com/:path*'
+            },
+            {
+                source: '/ingest/decide',
+                destination: 'https://us.i.posthog.com/decide'
+            }
+        ]
+    },
+    skipTrailingSlashRedirect: true
 } satisfies NextConfig
 
 // Injected content via Sentry wizard below
