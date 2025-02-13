c842c0628b7fbe8cb61217ec8b89ec607bfcb634
Kevin Zoltany <kzoltany@gmail.com>
2025-02-12 18:03:24 -0800

    Infinite Query

--- src/app/(core)/(home)/commissions.tsx
+++ src/app/(core)/(home)/commissions.tsx
@@ -1,61 +1,136 @@
 'use client'
 
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { motion } from 'motion/react'
+import { useIntersectionObserver } from '@uidotdev/usehooks'
 
 import NemuImage from '~/components/nemu-image'
+import { Badge, type BadgeProps } from '~/components/ui/badge'
+import {
+    Card,
+    CardTitle,
+    CardDescription,
+    CardHeader,
+    CardContent,
+    CardFooter
+} from '~/components/ui/card'
 import Loading from '~/components/ui/loading'
 
-import { api } from '~/trpc/react'
+import { api, type RouterOutputs } from '~/trpc/react'
 
-export function RandomCommissions() {
-    const { data, isLoading } = api.home.random_commissions.useQuery()
+export function InfiniteCommissions() {
+    const [ref, entry] = useIntersectionObserver({
+        root: null,
+        threshold: 0,
+        rootMargin: '0px'
+    })
 
-    if (isLoading) {
-        return (
-            <div className="mt-10 flex flex-1 flex-col items-center justify-center gap-2">
-                <Loading />
-            </div>
-        )
+    const query = api.home.get_commissions.useInfiniteQuery(
+        {
+            limit: 10
+        },
+        {
+            getNextPageParam: (last_page) => last_page.next_cursor
+        }
+    )
+
+    useEffect(() => {
+        if (entry?.isIntersecting && !query.isFetching && query.hasNextPage) {
+            void query.fetchNextPage()
+        }
+    }, [entry, query])
+
+    return (
+        <div className="mt-10 flex h-full w-full flex-1 flex-col items-center justify-center gap-5">
+            <motion.div
+                className="columns-1 gap-4 space-y-4 sm:columns-2 lg:columns-3 xl:columns-4"
+                animate={'visible'}
+                variants={{
+                    hidden: { opacity: 0, y: 20 },
+                    visible: {
+                        opacity: 1,
+                        y: 0,
+                        transition: {
+                            delayChildren: 0.3,
+                            staggerChildren: 0.2
+                        }
+                    }
+                }}
+                initial={'hidden'}
+            >
+                {query.data?.pages.map(({ res }) =>
+                    res.map((commission) => (
+                        <motion.div
+                            key={commission.id}
+                            initial={'hidden'}
+                            animate={'visible'}
+                            variants={{
+                                hidden: { opacity: 0, y: 20 },
+                                visible: { opacity: 1, y: 0 }
+                            }}
+                            className="break-inside-avoid"
+                        >
+                            <CommissionCard commission={commission} />
+                        </motion.div>
+                    ))
+                )}
+            </motion.div>
+            <div ref={ref}></div>
+            {query.isLoading && <Loading />}
+        </div>
+    )
+}
+
+function CommissionCard(props: {
+    commission: RouterOutputs['home']['get_commissions']['res'][number]
+}) {
+    let badge_variant: BadgeProps['variant'] = 'default'
+    switch (props.commission.availability) {
+        case 'closed':
+            badge_variant = 'destructive'
+            break
+        case 'waitlist':
+            badge_variant = 'warning'
+            break
+        default:
+            badge_variant = 'default'
+            break
     }
 
     return (
-        <div className="mt-10 columns-1 gap-4 sm:columns-2 lg:columns-3 xl:columns-4">
-            {data?.map((commission) => (
-                <div
-                    key={commission.id}
-                    className="mb-4 break-inside-avoid rounded-lg border bg-card p-4 shadow-sm"
-                >
-                    <Link
-                        prefetch={true}
-                        scroll={false}
-                        href={`/@${commission.artist.handle}/commission/${commission.slug}`}
-                    >
-                        <div className="aspect-square w-full overflow-hidden rounded-md">
-                            <NemuImage
-                                src={commission.featured_image}
-                                alt={commission.title}
-                                width={400}
-                                height={400}
-                                className="h-full w-full object-cover"
-                            />
-                        </div>
-                        <div className="mt-4">
-                            <h3 className="font-medium">{commission.title}</h3>
-                            <p className="mt-1 line-clamp-2 text-sm text-muted-foreground">
-                                {commission.description}
-                            </p>
-                            <div className="mt-2 flex items-center justify-between">
-                                <span className="text-sm font-medium">
-                                    {commission.price}
-                                </span>
-                                <span className="text-xs text-muted-foreground">
-                                    by @{commission.artist.handle}
-                                </span>
-                            </div>
-                        </div>
-                    </Link>
-                </div>
-            ))}
-        </div>
+        <Link
+            href={`/@${props.commission.artist.handle}/commission/${props.commission.slug}`}
+            scroll={false}
+        >
+            <Card>
+                <CardHeader className="aspect-square w-full overflow-hidden rounded-md">
+                    <NemuImage
+                        src={props.commission.featured_image}
+                        alt={props.commission.title}
+                        width={200}
+                        height={200}
+                        className="h-full w-full object-cover"
+                    />
+                </CardHeader>
+                <CardContent className="flex flex-col gap-2">
+                    <div className="flex w-full flex-col items-center justify-between sm:flex-row">
+                        <CardTitle>{props.commission.title}</CardTitle>
+                        <Badge variant={badge_variant}>
+                            {props.commission.availability}
+                        </Badge>
+                    </div>
+                    <CardDescription className="text-muted-foreground">
+                        {props.commission.description}
+                    </CardDescription>
+                </CardContent>
+                <CardFooter className="flex flex-col items-start">
+                    <span className="text-sm font-medium">{props.commission.price}</span>
+                    <span className="text-xs text-muted-foreground">
+                        by @{props.commission.artist.handle}
+                    </span>
+                </CardFooter>
+            </Card>
+        </Link>
     )
 }
--- src/app/(core)/(home)/page.tsx
+++ src/app/(core)/(home)/page.tsx
@@ -1,11 +1,11 @@
 import { HomeCarousel } from './carousel'
-import { RandomCommissions } from './commissions'
+import { InfiniteCommissions } from './commissions'
 
 export default function HomePage() {
     return (
         <div className="container mx-auto">
             <HomeCarousel />
-            <RandomCommissions />
+            <InfiniteCommissions />
         </div>
     )
 }
