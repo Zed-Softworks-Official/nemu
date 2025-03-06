3411669837a5c6ba25281cbfd99d8455d8513a50
Kevin Zoltany <kzoltany@gmail.com>
2025-03-05 22:58:20 -0800

    Updated cron routes

--- src/app/api/cron/uploadthing/route.ts
+++ src/app/api/cron/uploadthing/route.ts
@@ -5,7 +5,7 @@ import { tryCatch } from '~/lib/try-catch'
 import { redis } from '~/server/redis'
 import { utapi } from '~/server/uploadthing'
 
-async function process_event(expired_images: string[], expired_downloads: string[]) {
+async function processEvent(expired_images: string[], expired_downloads: string[]) {
     // Remove from redis
     const redis_promise = redis.zrem('product:images', ...expired_images)
     const redis_promise_2 = redis.zrem('product:downloads', ...expired_downloads)
@@ -17,35 +17,30 @@ async function process_event(expired_images: string[], expired_downloads: string
 }
 
 export async function POST(req: NextRequest) {
-    if (req.headers.get('Authorization') !== `Bearer ${env.UT_CRON_SECRET}`) {
+    if (req.headers.get('Authorization') !== `Bearer ${env.CRON_SECRET}`) {
         return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
     }
 
-    const current_time = Math.floor(Date.now() / 1000)
-    const expired_images = await redis.zrange<string[]>(
-        'product:images',
-        0,
-        current_time,
-        {
-            byScore: true
-        }
-    )
+    const currentTime = Math.floor(Date.now() / 1000)
+    const expiredImages = await redis.zrange<string[]>('product:images', 0, currentTime, {
+        byScore: true
+    })
 
-    const expired_downloads = await redis.zrange<string[]>(
+    const expiredDownloads = await redis.zrange<string[]>(
         'product:downloads',
         0,
-        current_time,
+        currentTime,
         {
             byScore: true
         }
     )
 
-    if (expired_images.length === 0 && expired_downloads.length === 0) {
+    if (expiredImages.length === 0 && expiredDownloads.length === 0) {
         return NextResponse.json({ received: true })
     }
 
     const doEventProcessing = async () => {
-        waitUntil(process_event(expired_images, expired_downloads))
+        waitUntil(processEvent(expiredImages, expiredDownloads))
     }
 
     const { error } = await tryCatch(doEventProcessing())
