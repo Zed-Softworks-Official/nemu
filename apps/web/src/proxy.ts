0dfa34faea5d2e182adcbe002b4bfe884367bf4b
Kevin Zoltany <kzoltany@gmail.com>
2026-07-18 23:28:41 -0700

    Actually fix clerk proxy

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+export default clerkMiddleware({
+    frontendApiProxy: {
+        enabled: true,
+    },
+})
 
 export const config = {
     matcher: [
@@ -8,5 +12,6 @@ export const config = {
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
         // Always run for API routes
         '/(api|trpc)(.*)',
+        '/__clerk/(.*)',
     ],
 }
