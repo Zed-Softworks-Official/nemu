efe721726ba355f401fa90cba7dea9b9b0440f74
Kevin Zoltany <kzoltany@gmail.com>
2026-07-19 03:30:09 -0700

    Removed layering

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -3,10 +3,13 @@ import '~/styles/globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ui } from '@clerk/ui'
 import { shadcn } from '@clerk/ui/themes'
+import { SidebarInset, SidebarProvider } from '@nemu/ui/components/sidebar'
 import { ThemeProvider } from '@nemu/ui/components/theme-provider'
 import { cn } from '@nemu/ui/lib/utils'
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
+import { Header } from '~/components/header/header'
+import { AppSidebar } from '~/components/sidebar/app-sidebar'
 import { env } from '~/env'
 
 export const metadata: Metadata = {
@@ -47,10 +50,29 @@ export default function RootLayout({
                         disableTransitionOnChange
                         enableSystem
                     >
-                        {children}
+                        <DashboardLayout>{children}</DashboardLayout>
                     </ThemeProvider>
                 </body>
             </html>
         </ClerkProvider>
+    )
+}
+
+function DashboardLayout(props: { children: React.ReactNode }) {
+    return (
+        <SidebarProvider
+            style={
+                {
+                    '--sidebar-width': 'calc(var(--spacing) * 72)',
+                    '--header-height': 'calc(var(--spacing) * 12)',
+                } as React.CSSProperties
+            }
+        >
+            <AppSidebar variant="inset" />
+            <SidebarInset className="md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-3 md:peer-data-[variant=inset]:m-3 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-2xl md:peer-data-[variant=inset]:border md:peer-data-[variant=inset]:border-border md:peer-data-[variant=inset]:shadow-lg">
+                <Header />
+                <div className="flex flex-1 flex-col p-5">{props.children}</div>
+            </SidebarInset>
+        </SidebarProvider>
     )
 }
